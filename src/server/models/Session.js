6e343da392bb434db9d2400ee0b6f54953917d14
Ricardo Barros <ricardofbarros@hotmail.com>
2015-08-29 20:21:52 +0100

    add userId to Session model

--- src/server/models/Session.js
+++ src/server/models/Session.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var Session = new Schema({
+  userId: Schema.Types.ObjectId,
   accessToken: String,
   game: {
     tableId: Schema.Types.ObjectId,
