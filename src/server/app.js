851a4feea5adcd2837763566b09bb6893974e9dd
Ricardo Barros <ricardofbarros@hotmail.com>
2015-08-29 21:33:00 +0100

    comment and do some other stuff on server app

--- src/server/app.js
+++ src/server/app.js
@@ -1,13 +1,40 @@
+// Dependencies
 var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var http = require('http');
+var SocketIo = require('socket.io');
 var routes = require('./routes');
 var boom = require('express-boom');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var config = require('../config');
 
-mongoose.connect('mongodb://localhost/blackjack');
+// Connect to mongo
+var mongoConnectionString = (function () {
+  var mongoCfg = config.mongoDB;
+  var connectionStr = '';
+
+  if (mongoCfg.user || mongoCfg.pass) {
+    connectionStr += mongoCfg.user + ':' + mongoCfg.pass + '@';
+  }
+
+  connectionStr += mongoCfg.host;
+
+  if (mongoCfg.port) {
+    connectionStr += ':' + mongoCfg.port;
+  }
+
+  if (mongoCfg.database) {
+    connectionStr += '/' + mongoCfg.database;
+  }
+
+  return connectionStr;
+})();
+mongoose.connect('mongodb://' + mongoConnectionString);
+
+// Create the services
+var app = express();
+var server = http.createServer(app);
+var io = SocketIo(server);
 
 io.on('connection', function (socket) {
   socket.on('create', function (room) {
@@ -15,9 +42,14 @@ io.on('connection', function (socket) {
   });
 });
 
+// Middlewares
 app.use(boom());
 app.use(bodyParser.json());
+
+// Server static content
 app.use('/public', express.static('public'));
+
+// Web API routes
 app.use('/api', routes);
 
 server.listen(3000);
