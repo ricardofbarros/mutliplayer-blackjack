ef5469ec2073c84024fc008b70afdbce522a337c
Ricardo Barros <ricardofbarros@hotmail.com>
2015-08-29 20:07:09 +0100

    add Table model

---
+++ src/server/models/Table.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Table = new Schema({
+  name: String,
+  createdDate: Date,
+  sittingPlayers: [{
+    userId: Schema.Types.ObjectId,
+    money: Number
+  }],
+  tableLimit: {
+    money: Number,
+    players: Number
+  }
+});
+
+module.exports = Table;
