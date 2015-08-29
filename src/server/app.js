87c0042fa43569da7d23b07b50eef71e8ec91470
Ricardo Barros <ricardofbarros@hotmail.com>
2015-08-29 20:46:37 +0100

    add boom and bodyParser as middlewares for express

--- src/server/app.js
+++ src/server/app.js
@@ -3,6 +3,8 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var routes = require('./routes');
+var boom = require('express-boom');
+var bodyParser = require('body-parser');
 
 io.on('connection', function (socket) {
   socket.on('create', function (room) {
@@ -10,6 +12,8 @@ io.on('connection', function (socket) {
   });
 });
 
+app.use(boom());
+app.use(bodyParser.json());
 app.use('/public', express.static('public'));
 app.use('/api', routes);
 
