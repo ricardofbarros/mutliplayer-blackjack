216559fa63ea078670e0f8a135274de9a7a022e0
Ricardo Barros <ricardofbarros@hotmail.com>
2015-08-30 16:43:19 +0100

    add sign-up route

--- src/client/routes.jsx
+++ src/client/routes.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {Route, DefaultRoute} from 'react-router';
 
 import App from './app';
-import Login from './containers/login';
+import LoginPage from './components/login/loginPage';
+import SignUpPage from './components/signUp/signUpPage';
 
 const routes = (
   <Route name='app' path='/' handler={App}>
-    <DefaultRoute handler={Login} />
+    <DefaultRoute handler={LoginPage} />
+    <Route name='sign-up' handler={SignUpPage} />
   </Route>
 );
 
