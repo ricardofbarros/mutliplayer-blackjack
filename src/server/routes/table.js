4bcd1351d1188c0fdd60b0be681522620a59c0b0
Ricardo Barros <ricardofbarros@hotmail.com>
2015-08-31 17:48:34 +0100

    add create new table endpoint

--- src/server/routes/table.js
+++ src/server/routes/table.js
@@ -1,3 +1,4 @@
+// Dependencies
 var express = require('express');
 var util = require('../util');
 var Table = require('../models/Table');
@@ -12,7 +13,49 @@ router.get('/', util.isAuthenticated, function (req, res) {
       return res.boom.badRequest(err);
     }
 
-    return res.end();
+    // var tableOut = tables.map()
+
+    return res.json();
+  });
+});
+
+// Create new table
+router.post('/', util.isAuthenticated, function (req, res) {
+  var payload = req.body;
+  var paramsKeys = Object.keys(payload);
+  var paramsRequired = [
+    'name',
+    'moneyLimit',
+    'playersLimit'
+  ];
+
+  // Check if all params required
+  // are in the payload
+  // If not return an error
+  var checkRequired = paramsRequired.every(function (param) {
+    return (paramsKeys.indexOf(param) > -1);
+  });
+  if (!checkRequired) {
+    return res.boom.badData('Missing params');
+  }
+
+  var table = new Table({
+    name: payload.name,
+    createdDate: new Date(),
+    tableLimit: {
+      money: parseInt(payload.moneyLimit, 10),
+      players: parseInt(payload.playersLimit, 10)
+    }
+  });
+
+  return table.save(function (err) {
+    if (err) {
+      return res.boom.badRequest(err);
+    }
+
+    return res.status(201).json({
+      message: 'Created table with success'
+    });
   });
 });
 
