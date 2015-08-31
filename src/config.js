2807ae575e59c2c1cd363e4480540456ea30464c
Ricardo Barros <ricardofbarros@hotmail.com>
2015-08-31 03:37:42 +0100

    add more stuff to config file

--- src/config.js
+++ src/config.js
@@ -8,6 +8,14 @@ config.mongoDB = {
   database: 'blackjack'
 };
 
+config.server = {
+  port: 3000
+};
+
+config.user = {
+  startingMoney: 10000
+};
+
 // This is the only property that
 // will be available to the client-side
 config.client = {
