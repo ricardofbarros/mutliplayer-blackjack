c1a9b3556fb4dcc29f45a186df1f2b3e1d77d83a
Ricardo Barros <ricardofbarros@hotmail.com>
2015-08-31 23:25:25 +0100

    remove props from table model

--- src/server/models/Table.js
+++ src/server/models/Table.js
@@ -4,16 +4,11 @@ var Schema = mongoose.Schema;
 var Table = new Schema({
   name: String,
   createdDate: Date,
-  sittingPlayers: [{
-    userId: Schema.Types.ObjectId,
-    money: Number
-  }],
   tableLimit: {
     money: Number,
     players: Number
   },
-  numberOfDecks: Number,
-  _cards: Array
+  numberOfDecks: Number
 });
 
 module.exports = mongoose.model('Table', Table);
