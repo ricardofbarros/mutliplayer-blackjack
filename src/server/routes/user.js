e02e45eae5ce326ce4bacaf0239b7220f07c68dc
Ricardo Barros <ricardofbarros@hotmail.com>
2015-08-31 04:24:57 +0100

    add validation to create user route

--- src/server/routes/user.js
+++ src/server/routes/user.js
@@ -7,6 +7,29 @@ var config = require('../config');
 
 // Create new user
 router.post('/', function (req, res) {
+  var payload = req.params;
+  var paramsKeys = Object.keys(payload);
+  var paramsRequired = [
+    'username',
+    'password',
+    'confirmPassword'
+  ];
+
+  // Check if all params required
+  // are in the payload
+  // If not return an error
+  var checkRequired = paramsRequired.every(function (param) {
+    return (paramsKeys.indexOf(param) > -1);
+  });
+  if (!checkRequired) {
+    return res.boom.badData('Missing params.');
+  }
+
+  // Sane check
+  if (payload.password !== payload.confirmPassword) {
+    return res.boom.badData('Password doesn\'t match with Confirm Password');
+  }
+
   return User.findOne({ username: req.params.username }, function (err, user) {
     if (err) {
       return res.boom.badRequest(err);
