8115ea6ba84624a7e134df0092a2da6474b3f0ab
Ricardo Barros <ricardofbarros@hotmail.com>
2015-09-01 10:11:59 +0100

    misc

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -5,13 +5,13 @@ import React from 'react';
 import Router from 'react-router';
 import { Provider } from 'react-redux';
 import routes from './routes';
-import reduxStore from './store/redux';
+import ReduxStore from './store/redux';
 import '../public/css/style.css';
 import 'toastr/build/toastr.css';
 
 Router.run(routes, Router.HistoryLocation, (Handler, state) => {
   React.render(
-    <Provider store={reduxStore}>
+    <Provider store={ReduxStore}>
       {() => <Handler routerState={state} />}
     </Provider>,
     document.getElementById('content')
