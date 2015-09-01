98560fe2ac7cb3032a5a82556ff4d7eb602c7cf0
Ricardo Barros <ricardofbarros@hotmail.com>
2015-09-01 10:57:14 +0100

    delete unused stuff

--- src/client/actions/user.js
+++
@@ -1,9 +0,0 @@
-import { createAction } from 'redux-actions';
-
-export const APP_HYDRATE_USER = 'APP_HYDRATE_USER';
-
-let hydrateUser = createAction(APP_HYDRATE_USER, (user) => {
-  return user;
-});
-
-export default { hydrateUser };
