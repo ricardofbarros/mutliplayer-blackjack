491902ff20cd27f537765458fe68d255f8ca7f6c
Ricardo Barros <ricardofbarros@hotmail.com>
2015-09-01 06:34:10 +0100

    add finished signUp page

--- src/client/components/signUp/signUpPage.jsx
+++ src/client/components/signUp/signUpPage.jsx
@@ -1,7 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Navigation } from 'react-router';
 import SignUpForm from './signUpForm';
 import FormBox from '../common/formBox/formBox';
+import UserApi from '../../api/user';
+import reactMixin from 'react-mixin';
+import toastr from 'toastr';
 
+@reactMixin.decorate(Navigation)
 class SignUpPage extends Component {
   constructor () {
     super();
@@ -10,18 +15,35 @@ class SignUpPage extends Component {
     };
   }
 
-  setLoginState (e) {
+  setSignUpState (e) {
     let field = e.target.name;
     let value = e.target.value;
-    this.state.login[field] = value;
-    return this.setState({login: this.state.login});
+    this.state.data[field] = value;
+    return this.setState({data: this.state.data});
+  }
+
+  async signUp (user, pass, confirmPass, e) {
+    e.preventDefault();
+    let result = await UserApi.createNew(user, pass, confirmPass);
+
+    if (result.error) {
+      toastr.error(result.data.message);
+    } else {
+      toastr.success(result.data.message);
+      this.transitionTo('app');
+    }
   }
 
   render () {
     return (
       <FormBox
         header='Sign Up'
-        form={<SignUpForm data={this.state.data} onChange={this.setLoginState}/>}/>
+        form={
+          <SignUpForm
+            data={this.state.data}
+            onChange={this.setSignUpState.bind(this)}
+            onSave={this.signUp.bind(this)} />
+        } />
     );
   }
 
