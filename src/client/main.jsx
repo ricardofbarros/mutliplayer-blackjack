ea5cbb64820cd76844f8689427b5ffeeb24352f4
Ricardo Barros <ricardofbarros@hotmail.com>
2015-09-01 09:49:11 +0100

    update main.jsx

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -1,21 +1,17 @@
 import 'bootstrap-webpack';
+import 'regenerator/runtime';
+import 'babel-core/polyfill';
 import React from 'react';
 import Router from 'react-router';
 import { Provider } from 'react-redux';
 import routes from './routes';
-import { createStore } from 'redux';
-import reducers from './reducers';
+import reduxStore from './store/redux';
 import '../public/css/style.css';
-
-function configureStore (initialState) {
-  return createStore(reducers, initialState);
-}
-
-const store = configureStore();
+import 'toastr/build/toastr.css';
 
 Router.run(routes, Router.HistoryLocation, (Handler, state) => {
   React.render(
-    <Provider store={store}>
+    <Provider store={reduxStore}>
       {() => <Handler routerState={state} />}
     </Provider>,
     document.getElementById('content')
