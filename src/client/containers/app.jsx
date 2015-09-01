41bf6a9af0938e9f52defc2d7d4a8bab6c12f78c
Ricardo Barros <ricardofbarros@hotmail.com>
2015-09-01 09:47:26 +0100

    refactor app to use redux

--- src/client/containers/app.jsx
+++ src/client/containers/app.jsx
@@ -1,22 +1,65 @@
 import React, {Component} from 'react';
 import {RouteHandler} from 'react-router';
-import Header from './components/common/header';
+import { connect } from 'react-redux';
+import Header from '../components/common/header';
+import Cookies from '../store/cookies';
+import SessionActions from '../actions/session';
+import UserActions from '../actions/user';
 
 class App extends Component {
+  propTypes = {
+    getUserInfo: React.PropTypes.func.isRequired,
+    hydrateUser: React.PropTypes.func.isRequired,
+    hydrateSession: React.PropTypes.func.isRequired,
+    user: React.PropTypes.object
+  }
+
+  async componentDidMount () {
+    let accessToken = Cookies.get('accessToken');
+    if (accessToken) {
+      let result = await this.props.getUserInfo(accessToken);
+
+      if (!result.payload) {
+        return;
+      }
+
+      // User is currently playing a game
+      if (result.payload.table) {
+        // Do something
+      } else {
+        this.props.hydrateUser(result.payload.user);
+        this.props.hydrateSession(result.payload.session);
+      }
+    }
+  }
 
   render () {
+    var user = this.props.user;
+
     return (
       <div>
         {(() => {
-          if (true) {
-            return <Header name='Ricardo' money={1000}/>;
+          if (user.id) {
+            return <Header name={user.username} money={user.accountBalance}/>;
           }
         })()}
-        <RouteHandler/>
+        <div className='container'>
+          <RouteHandler/>
+        </div>
       </div>
     );
   }
-
 }
 
-export default App;
+function mapStateToProps (state) {
+  return {
+    user: state.user,
+    tables: state.tables,
+    session: state.session
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  {...SessionActions, ...UserActions}
+)(App);
