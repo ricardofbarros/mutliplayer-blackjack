59b53c0290559911fa80e9aeb075b339ff45196c
Ricardo Barros <ricardofbarros@hotmail.com>
2015-09-01 06:33:54 +0100

    add user api finished (client)

--- src/client/api/user.js
+++ src/client/api/user.js
@@ -1,13 +1,37 @@
-import {client as config} from 'config';
+import config from 'clientconfig';
 import axios from 'axios';
 import url from 'url';
 
-function createNew (username, password, confirmPassword) {
-  return axios.post(url.resolve(config.baseUrl, '/api/user'), {
-    username,
-    password,
-    confirmPassword
-  });
+const MISSING_PARAMS = config.apiMsgState.user.MISSING_PARAMS;
+const USER_EXISTS = config.apiMsgState.user.USER_EXISTS;
+const PASSWORD_MATCH = config.apiMsgState.user.PASSWORD_MATCH;
+
+async function createNew (username, password, confirmPassword) {
+  let result;
+  try {
+    result = await axios.post(url.resolve(config.baseUrl, '/api/user'), {
+      username,
+      password,
+      confirmPassword
+    });
+  } catch (e) {
+    if (!e.data || !e.data.message) {
+      throw e;
+    }
+
+    switch (e.data.message) {
+      case MISSING_PARAMS:
+      case USER_EXISTS:
+      case PASSWORD_MATCH:
+        result = e;
+        result.error = true;
+        break;
+      default:
+        throw e;
+    }
+  }
+
+  return result;
 }
 
 export default { createNew };
