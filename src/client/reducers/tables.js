21eb0f5f385076455f0f6a3c6d0a0d8b99d2ab53
Ricardo Barros <ricardofbarros@hotmail.com>
2015-09-02 12:07:02 +0100

    fix table reducer

--- src/client/reducers/tables.js
+++ src/client/reducers/tables.js
@@ -3,7 +3,7 @@ import { APP_HYDRATE_TABLES } from '../actions/tables';
 export default function tables (state = 0, action) {
   switch (action.type) {
     case APP_HYDRATE_TABLES:
-      return Object.assign({}, state, action.payload);
+      return Object.assign([], state, action.payload);
     default:
       return state;
   }
