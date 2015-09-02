d2ec3a24e9fa7a1d4f6465cb67659b54d0b5eda0
Ricardo Barros <ricardofbarros@hotmail.com>
2015-09-02 18:31:27 +0100

    add new api msg state to api wrapper

--- src/client/api/user.js
+++ src/client/api/user.js
@@ -6,6 +6,8 @@ import apiCallWrapper from './common/apiCallWrapper';
 const MISSING_PARAMS = config.apiMsgState.misc.MISSING_PARAMS;
 const USER_EXISTS = config.apiMsgState.user.USER_EXISTS;
 const PASSWORD_MATCH = config.apiMsgState.user.PASSWORD_MATCH;
+const INVALID_USER_FIELD = config.apiMsgState.user.INVALID_USER_FIELD;
+const INVALID_PASS_FIELD = config.apiMsgState.user.INVALID_PASS_FIELD;
 
 async function createNew (username, password, confirmPassword) {
   let promise = axios.post.bind(axios, url.resolve(config.baseUrl, '/api/user'), {
@@ -14,7 +16,7 @@ async function createNew (username, password, confirmPassword) {
     confirmPassword
   });
 
-  return await apiCallWrapper(promise, [MISSING_PARAMS, USER_EXISTS, PASSWORD_MATCH]);
+  return await apiCallWrapper(promise, [MISSING_PARAMS, USER_EXISTS, PASSWORD_MATCH, INVALID_USER_FIELD, INVALID_PASS_FIELD]);
 }
 
 export default { createNew };
