a7f62f222e37608027ba15f2ce95d80ecee897ee
Ricardo Barros <ricardofbarros@hotmail.com>
2015-09-02 20:08:49 +0100

    inject meta in action when a token is expired

--- src/client/actions/common/createAction.js
+++ src/client/actions/common/createAction.js
@@ -1,3 +1,5 @@
+import TOKEN_EXPIRED from '../../api/common/tokenExpired';
+
 export default function createAction (type, actionCreator, mapFn) {
   return async (...a) => {
     let action = {
@@ -8,6 +10,11 @@ export default function createAction (type, actionCreator, mapFn) {
     action.payload = result.data;
 
     if (result.error) {
+      if (result.data && result.data.message && result.data.message === TOKEN_EXPIRED) {
+        action.meta = {
+          tokenExpired: true
+        };
+      }
       action.error = true;
     } else if (!result.error && typeof mapFn === 'function') {
       let mapResult = [action.payload].map(mapFn);
