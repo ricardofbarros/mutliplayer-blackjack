9d3545a1a0bcfd6ba00b6d014adeec65a5e655d8
Ricardo Barros <ricardofbarros@hotmail.com>
2015-09-02 14:54:52 +0100

    finish common validation user

--- src/common/validation.js
+++ src/common/validation.js
@@ -1,9 +1,14 @@
 var validation = {};
 
+// Regex strings
+var alphanumeric = '^[a-zA-Z0-9_]*$';
+
 validation.user = function (apiMsgState, payload) {
   // Constants
   var MISSING_PARAMS = apiMsgState.misc.MISSING_PARAMS;
   var PASSWORD_MATCH = apiMsgState.user.PASSWORD_MATCH;
+  var INVALID_USER_FIELD = apiMsgState.user.INVALID_USER_FIELD;
+  var INVALID_PASS_FIELD = apiMsgState.user.INVALID_PASS_FIELD;
 
   var paramsRequired = [
     'username',
@@ -20,6 +25,14 @@ validation.user = function (apiMsgState, payload) {
     return PASSWORD_MATCH;
   }
 
+  var re = new RegExp(alphanumeric, 'g');
+  if (!re.test(payload.username)) {
+    return INVALID_USER_FIELD;
+  }
+  if (!re.test(payload.password)) {
+    return INVALID_PASS_FIELD;
+  }
+
   return false;
 };
 
@@ -34,6 +47,14 @@ validation.table = function (apiMsgState, payload) {
     'numberOfDecks',
     'buyin'
   ];
+
+//   {
+//     "name": "test",
+//     "moneyLimit": "1000",
+//     "playersLimit": "6",
+//     "numberOfDecks": "6",
+//     "buyin": "500"
+// }
 
   if (!checkParamsRequired(payload, paramsRequired)) {
     return MISSING_PARAMS;
