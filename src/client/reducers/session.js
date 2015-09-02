7a8a75777d49c52b127a78d0ebb679139b0586f2
Ricardo Barros <ricardofbarros@hotmail.com>
2015-09-02 20:09:27 +0100

    protect session and table reducer from action that have errros

--- src/client/reducers/session.js
+++ src/client/reducers/session.js
@@ -1,6 +1,10 @@
 import { GET_USER_INFO } from '../actions/session';
 
 export default function session (state = 0, action) {
+  if (action && action.error) {
+    return state;
+  }
+
   switch (action.type) {
     case GET_USER_INFO:
       return Object.assign({}, state, action.payload);
--- src/client/reducers/tables.js
+++ src/client/reducers/tables.js
@@ -1,28 +1,27 @@
 import { APP_HYDRATE_TABLES, ADD_TABLE, REMOVE_TABLE, UPDATE_TABLE } from '../actions/tables';
+import { OrderedMap } from 'immutable';
 
 export default function tables (state = 0, action) {
-  if (typeof state !== 'object') {
+  if (action && action.error) {
     return state;
   }
 
   switch (action.type) {
     case APP_HYDRATE_TABLES:
-      let hydrateData = new Map();
+      let hydrateData = {};
       action.payload.forEach(function (t) {
-        hydrateData.set(t.id, t);
+        hydrateData[t.id] = t;
       });
 
-      return hydrateData;
+      return OrderedMap(hydrateData);
     case ADD_TABLE:
-      let data = [];
-      state.forEach(function (d) {
-        data.push(d);
-      });
-      data.push(action.payload);
-      return data;
+      return state.set(action.payload.id, action.payload);
     case REMOVE_TABLE:
-
+      return state.delete(action.payload.id);
     case UPDATE_TABLE:
+      return state.update(action.payload.id, () => {
+        return action.payload;
+      });
     default:
       return state;
   }
