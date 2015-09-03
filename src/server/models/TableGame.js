42b96b596ac59098f7b616ba9d555fc210b8b64e
Ricardo Barros <ricardofbarros@hotmail.com>
2015-09-03 16:56:35 +0100

    remove sitout static method

--- src/server/models/TableGame.js
+++ src/server/models/TableGame.js
@@ -41,35 +41,8 @@ TableGame.static.removeSitOut = function (tableId, userId, cb) {
   return this.update({
     tableId: tableId
   }, {
-    $pull: { 'playersSeatedOut.userId': userId }
+    $pull: { playersSeatedOut: { userId: userId } }
   }, cb);
-};
-
-TableGame.static.kickAndIncrementSitOuts = function (tableId, cb) {
-  return this.findOne({ tableId: tableId }, function (err, tableGame) {
-    if (err || !tableGame) {
-      return cb(err || new Error('Didn\'t find the table'));
-    }
-
-    // remove / kick players
-    var playersKicked = [];
-    tableGame.playersSeatedOut.filter(function (player) {
-      if (!player.rounds < 6) {
-        playersKicked.push(player.userId);
-        return false;
-      }
-      return true;
-    });
-
-    tableGame.playersSeatedOut.map(function (player) {
-      player.rounds++;
-      return player;
-    });
-
-    return tableGame.save(function (err) {
-      return cb(err, tableGame.playersSeatedOut, playersKicked);
-    });
-  });
 };
 
 module.exports = mongoose.model('TableGame', TableGame);
